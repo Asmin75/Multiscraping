a548a9e66df87c4786fe696aa55a2438cbc27e7b
Asmin75 <asminrai7@gmail.com>
2019-08-04 12:52:22 +0545

    with mapping urls

--- multiurlscrapy/spiders/crawler.py
+++ multiurlscrapy/spiders/crawler.py
@@ -1,33 +1,38 @@
 import scrapy
 from scrapy.http.request import Request
-from ..items import MultiurlscrapyItem
+
 
 class MultiSpider(scrapy.Spider):
     name = 'multiple'
-    # start_urls = [
-    #     "https://starternepal.com/",
-    #     "https://www.ntc.net.np/"
-    # ]
-    # def start_requests(self):
-    #     with open('urls.txt', 'rb') as urls:
-    #         for url in urls:
-    #             yield Request(str(url), callback=self.parse)
-    #
-    # def parse(self, response):
-    #     for text in response.css('div'):
-    #         item = MultiurlscrapyItem()
-    #         item['paragraph'] = text.css("p::text").extract()
-    #         yield item
-    with open('urls.txt') as f:
-        links = f.readlines()
-        links = list(map(lambda x: x.strip(), links))
-    start_urls = links
+    def start_requests(self):
+
+        with open('urls.txt') as f:
+            links = f.readlines()
+            links = list(map(lambda x: x.strip(), links))
+            for link in links:
+                yield Request(link, meta={'start_urls': link}, callback=self.parse)
+
+        # start_urls = links
 
     def parse(self, response):
-        # for text in response.css('div'):
+        # for url in response.meta['start_urls']:
+        #     start_url = url
+        start_url = response.meta['start_urls']
+        link_list = response.css('ul li a ::text').extract()
         paragraph = response.css('p::text').extract()
-        paragraph = list(map(str.strip, paragraph))
+        h3 = response.css('h3::text').extract()
+        h2 = response.css('h2::text').extract()
+
+        # start_url = list(map(str.strip, start_url))
+        # link_list = list(map(str.strip, link_list))
+        # paragraph = list(map(str.strip, paragraph))
+        # h3 = list(map(str.strip, h3))
+        # h2 = list(map(str.strip, h2))
+
         yield {
+            'url': start_url,
+            'link_list': link_list,
             'paragraph': paragraph,
-            # 'Main Info': description,
+            'h3': h3,
+            'h2': h2,
         }
