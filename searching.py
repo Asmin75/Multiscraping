3a716099670d4cf0708dedb317e96af9267b0112
Asmin75 <asminrai7@gmail.com>
2019-08-07 16:38:10 +0545

    Searched keyword and display full string with its related urls

--- searching.py
+++ searching.py
@@ -4,7 +4,10 @@ with open("items.json") as f:
     data = json.load(f)
     search = str(input("Enter word to find reference link:"))
     for item in data:
-        match = re.findall(search, str(item['h2'] or item['h3']))
-        if match:
-            print("word matched: %s ==> url: %s" % (search, item['url']))
+        v = list(item.values())
+        for i in range(len(v)):
+            for j in range(len(v[i])):
+                match = re.search(search, v[i][j]) or re.search(search, v[i][j])
+                if match:
+                    print("word matched: %s ==> url: %s" % (v[i][j], item['url']))
 
